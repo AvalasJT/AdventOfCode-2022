e5c84947055a4a58b95d165331e74b47318fb840
AvalasJT <79806487+AvalasJT@users.noreply.github.com>
2022-12-06 17:59:44 +0100

    Removed while True loop

--- Day_06.py
+++ Day_06.py
@@ -1,14 +1,9 @@
-# Part 1 
-def get_marker(n):
-    pos = n
-    while True:
-        if len(set(signal[pos-n:pos])) == n: break
-        pos += 1
-    return pos
+# Part 1 + 2
+def get_marker(n, sig):
+    for pos in range(n, len(sig)+1):
+        if len(set(sig[pos-n:pos])) == n: return pos
 
 with open('input.txt', 'r') as f: signal = f.readline().strip('\n')
 
-print(get_marker(4))
-
-# Part 2
-print(get_marker(14))
+print(get_marker(4, signal))
+print(get_marker(14, signal))
